987b4c488c55e1dad57ad113034560c932aadff5
Charek Chen <charekc@ca.ibm.com>
2021-03-21 16:44:10 -0400

    Second Commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
-
+// Making a comment
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
 	}
